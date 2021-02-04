271b5c1b28f6c2692b418682447da312a73b7f24
thkim4729 <71238632+thkim4729@users.noreply.github.com>
2021-02-04 14:53:10 +0900

    change output directory

---
+++ docs/precache-manifest.e4ea84ad8456c14ca3ead5dad1a134a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b2775b6909a76444f6",
+    "url": "/git-test/css/app.9cbd5599.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/git-test/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e66518bcbba46bc56b44fa83e393c36a",
+    "url": "/git-test/index.html"
+  },
+  {
+    "revision": "f34ffcb314b215d8a752",
+    "url": "/git-test/js/about.d68885b5.js"
+  },
+  {
+    "revision": "79b2775b6909a76444f6",
+    "url": "/git-test/js/app.cbb8a35e.js"
+  },
+  {
+    "revision": "dcdf5be343b133eaabed",
+    "url": "/git-test/js/chunk-vendors.78b60929.js"
+  },
+  {
+    "revision": "39fa1bdc5263974509333d58cbd9c888",
+    "url": "/git-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/git-test/robots.txt"
+  }
+]);
